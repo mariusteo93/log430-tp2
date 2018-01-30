ba5423f655ddf3caabf974f7101873032ecb35e2
Marius Teodorescu <Marius.Teodorescu@etsmtl.net>
2018-01-30 09:42:49 -0500

    Initilisation tp2

---
+++ log430-lab1/src/main/java/loanutils/ChangeListener.java
@@ -0,0 +1,21 @@
+/*
+ * Components that are interested in LoanItem changes should implement this interface.
+ */
+package loanutils;
+
+import loanutils.LoanItem;
+
+/**
+ * Components that are interested in LoanItem changes should implement this interface.
+ *
+ * @author jean-blas imbert
+ */
+public interface ChangeListener {
+
+    /**
+     * Function should be fired when the item has changed
+     *
+     * @param pItem the changed item
+     */
+    void itemChanged(final LoanItem pItem);
+}
---
+++ log430-lab1/src/main/java/loanutils/DiffListener.java
@@ -0,0 +1,22 @@
+/*
+ * Components that are interested in LoanItem differenciations should implement this interface.
+ */
+package loanutils;
+
+import loanutils.LoanItem;
+
+/**
+ * Components that are interested in LoanItem differenciations should implement this interface.
+ *
+ * @author jean-blas imbert
+ */
+public interface DiffListener {
+
+    /**
+     * Compute the real difference between two loan items
+     *
+     * @param pItem1 the first loan item
+     * @param pItem2 the second loan item
+     */
+    void itemDiffed(final LoanItem pItem1, final LoanItem pItem2);
+}
